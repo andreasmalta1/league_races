5c7c914068e0211faac7263c35d09071ba5be8c6
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-10-24 13:59:56 +0200

    Updated main app

--- main_app/__init__.py
+++ main_app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, render_template
 from dotenv import load_dotenv
 from os import getenv
 
@@ -9,6 +9,4 @@ app.config["SECRET_KEY"] = getenv("SECRET_KEY")
 
 @app.route('/video')
 def serve_video():
-    # Define the path to the latest generated video
-    video_path = "path/to/your/generated_video.mp4"
-    return send_file(video_path, as_attachment=True)
+    return render_template("video.html", file_name = "epl_clubs_final.mp4")
